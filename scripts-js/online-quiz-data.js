1292aedc1e3f79ce21eddc692a8d527d1e729bb0
ISAAC227 <isaacsebuwufu@gmail.com>
2024-01-11 22:54:11 +0100

    Separating data and functionality

---
+++ scripts-js/online-quiz-data.js
@@ -0,0 +1,13 @@
+
+var quizData = [
+  {
+    question: "Who is the founder of Reggae Music?",
+    options: ["Bob Marley", "Toots and Maytals", "Lee Perry", "Shaggy"],
+    correctAnswer: "Toots and Maytals",
+    subsquestions: {
+    question: "In 1968, the band Toots and the Maytals recorded their first Reggae song titled?",
+    options: ["Reggae", "Jah", "54-46", "Buffalo"],
+    correctAnswer: "54-46"
+    }
+  }
+]
--- scripts-js/online-quiz.js
+++ scripts-js/online-quiz.js
@@ -1,19 +1,4 @@
 
-var quizData = [
-  {
-    question: "Who is the founder of Reggae Music?",
-    options: ["Bob Marley", "Toots and Maytals", "Lee Perry", "Shaggy"],
-    correctAnswer: "Toots and Maytals",
-    subsquestions: {
-    question: "In 1968, the band Toots and the Maytals recorded their first Reggae song titled?",
-    options: ["Reggae", "Jah", "54-46", "Buffalo"],
-    correctAnswer: "54-46"
-    }
-  }
-]
-// =============================DATA =================================
-
-
 let currentQuestionIndex = 0;
 let score = 0;
 
